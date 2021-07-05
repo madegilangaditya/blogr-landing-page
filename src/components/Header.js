d728a2e8c59f076da7a1163e9dc7fa0d3c911ee5
gilang aditya <madegilangaditya32@gmail.com>
2021-07-05 17:34:49 +0800

    continue styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,52 +6,54 @@ import Button from './Button'
 const Header = () => {
     return (
         <header>
-           <div className="row flex">
-                <div className="col col-left">
-                    <div className="logo-wrap">
-                        <img src={logo} alt="Logo" />
+            <div className="container">
+                <div className="row flex">
+                    <div className="col col-left">
+                        <div className="logo-wrap">
+                            <img src={logo} alt="Logo" />
+                        </div>
+                    </div>
+                    <div className="col col-right">
+                        <div className="mobile-menu-icon">
+                            <img src={mobileMenu} alt="" />
+                        </div>
+                        <nav>
+                            <div className="menu-left">
+                                <ul>
+                                    <li className="has-child"><a href="#">Product</a></li>
+                                        <ul className="child-menu">
+                                            <li><a href="#">Overview</a></li>
+                                            <li><a href="#">Pricing</a></li>
+                                            <li><a href="#">Marketplace</a></li>
+                                            <li><a href="#">Features</a></li>
+                                            <li><a href="#">Integrations</a></li>
+                                        </ul>
+                                    <li className="has-child"><a href="#">Company</a></li>
+                                        <ul className="child-menu">
+                                            <li><a href="#">About</a></li>
+                                            <li><a href="#">Team</a></li>
+                                            <li><a href="#">Blog</a></li>
+                                            <li><a href="#">Careers</a></li>
+                                        </ul>
+                                    <li className="has-child"><a href="#">Connect</a></li>
+                                        <ul className="child-menu">
+                                            <li><a href="#">Contact</a></li>
+                                            <li><a href="#">Newsletter</a></li>
+                                            <li><a href="#">LinkedIn</a></li>
+                                        </ul>
+                                </ul>
+                            </div>
+                            <div className="menu-right">
+                                <ul>
+                                    <li><a href="#">Login</a></li>
+                                    <li><Button name='btn-light' title='Sign Up'/></li>
+                                </ul>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-                <div className="col col-right">
-                    <div className="mobile-menu-icon">
-                        <img src={mobileMenu} alt="" />
-                    </div>
-                    <nav>
-                        <div className="menu-left">
-                            <ul>
-                                <li className="has-child"><a href="#">Product</a></li>
-                                    <ul className="child-menu">
-                                        <li><a href="#">Overview</a></li>
-                                        <li><a href="#">Pricing</a></li>
-                                        <li><a href="#">Marketplace</a></li>
-                                        <li><a href="#">Features</a></li>
-                                        <li><a href="#">Integrations</a></li>
-                                    </ul>
-                                <li className="has-child"><a href="#">Company</a></li>
-                                    <ul className="child-menu">
-                                        <li><a href="#">About</a></li>
-                                        <li><a href="#">Team</a></li>
-                                        <li><a href="#">Blog</a></li>
-                                        <li><a href="#">Careers</a></li>
-                                    </ul>
-                                <li className="has-child"><a href="#">Connect</a></li>
-                                    <ul className="child-menu">
-                                        <li><a href="#">Contact</a></li>
-                                        <li><a href="#">Newsletter</a></li>
-                                        <li><a href="#">LinkedIn</a></li>
-                                    </ul>
-                            </ul>
-                        </div>
-                        <div className="menu-right">
-                            <ul>
-                                <li><a href="#">Login</a></li>
-                                <li><Button name='btn-light' title='Sign Up'/></li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                <Banner/>
             </div>
-            <Banner/>
         </header>
     )
 }
